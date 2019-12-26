1773ceed3efb86bbfd801a167aa547cbcd5371f7
Vasiliy <Freeneos@icloud.com>
2019-12-26 18:41:49 +0300

    Initial Commit

---
+++ LibtessSPMExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LibtessSPMExample
+//
+//  Created by Vasiliy Shaydullin on 26.12.2019.
+//  Copyright © 2019 Vasiliy Shaydullin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
